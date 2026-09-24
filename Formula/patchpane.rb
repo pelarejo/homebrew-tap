709c8cf7fd4dc1df8345f735e7531c243da2c7f1
Pelayo PAREJO PAGADOR <pelayo.parejopagador@gmail.com>
2026-09-24 18:31:41 +0000

    patchpane: add 0.1.2_1 bottle.

--- Formula/patchpane.rb
+++ Formula/patchpane.rb
@@ -7,6 +7,12 @@ class Patchpane < Formula
   revision 1
   head "https://github.com/pelarejo/Patchpane.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/pelarejo/homebrew-tap/releases/download/patchpane-0.1.2_1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "5bdc2c5ee68cbc57aeafd21918c51dd33e8bf6c8c3488637bb2b76fbe947c2cc"
+    sha256 cellar: :any,                 x86_64_linux: "fc4faf030639cdc83386d9a6a7ab70cf9c735f04a65e1c5cd1107f11ea5138be"
+  end
+
   depends_on "rust" => :build
 
   def install
